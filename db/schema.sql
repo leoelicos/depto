d40d2dca1093c1a8a681c38a8eab2e10050f578a
Leo Wong <leoelicos@gmail.com>
2022-05-11 13:24:59 +1000

    Create table employee

--- db/schema.sql
+++ db/schema.sql
@@ -18,6 +18,13 @@ CREATE TABLE role (
   ON DELETE SET NULL
 );
 
-
-
-
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+);
