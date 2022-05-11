d398573e2165d5d7c8179108d40f96ab2d770819
Leo Wong <leoelicos@gmail.com>
2022-05-11 23:58:59 +1000

    Add comment header

---
+++ utils.js/postFetch.js
@@ -0,0 +1,11 @@
+/* 
+postFetch.js
+
+This utility file contains helper POST fetch methods
+
+addDepartment(name)
+addRole(title, salary, department_id)
+addEmployee(first_name, last_name, role_id, manager_id)
+
+Copyright Leo Wong 2022
+*/
