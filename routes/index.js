456eda96bc184da65e33f5d8828e052bcfc53498
Leo Wong <leoelicos@gmail.com>
2022-05-11 17:33:43 +1000

    Import express

--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,6 @@ This script contains necessary code to handle routes to /api
 
 Copyright Leo Wong 2022
 */
+
+// express is an npm library package which links client requests to server responses
+const express = require('express');
