17907467a5b827e10137b4866286ef7c1dd47cec
Leo Wong <leoelicos@gmail.com>
2022-05-11 23:59:11 +1000

    addDepartment

--- utils.js/postFetch.js
+++ utils.js/postFetch.js
@@ -9,3 +9,22 @@ addEmployee(first_name, last_name, role_id, manager_id)
 
 Copyright Leo Wong 2022
 */
+
+const addDepartment = (name) =>
+	fetch('/api/department', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			name: name,
+		}),
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			console.log('Successful POST request:', data);
+			return data;
+		})
+		.catch((error) => {
+			console.error('Error in POST request:', error);
+		});
