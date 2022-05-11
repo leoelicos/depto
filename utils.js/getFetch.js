ed2754531a983e2d4c65acc91e2f3ac20b330aae
Leo Wong <leoelicos@gmail.com>
2022-05-11 23:48:23 +1000

    getDepartments

--- utils.js/getFetch.js
+++ utils.js/getFetch.js
@@ -5,3 +5,10 @@ This utility file contains helper GET fetch functions
 
 Copyright Leo Wong 2022
 */
+
+const getDepartments = () =>
+	fetch('/api/view/departments', {
+		method: 'GET',
+	})
+		.then((res) => res.json())
+		.then((data) => data);
