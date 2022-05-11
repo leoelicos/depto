73b8e4495080764c1bbba3c889b26da54d520e6e
Leo Wong <leoelicos@gmail.com>
2022-05-12 00:12:14 +1000

    updateEmployeeRole

--- utils.js/putFetch.js
+++ utils.js/putFetch.js
@@ -8,3 +8,23 @@ updateEmployeeManager(employee_id, manager_id)
 
 Copyright Leo Wong 2022
 */
+
+const updateEmployeeRole = (employee_id, role_id) =>
+	fetch('/api/employeeRole', {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			employee_id: employee_id,
+			role_id: role_id,
+		}),
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			console.log('Successful PUT request:', data);
+			return data;
+		})
+		.catch((error) => {
+			console.error('Error in PUT request:', error);
+		});
