786f5cb463584bfc98e07ba668fc68ae349ad2f7
Leo Wong <leoelicos@gmail.com>
2022-05-11 23:11:13 +1000

    Add comment header

---
+++ routes/delete.js
@@ -0,0 +1,11 @@
+/* 
+delete.js
+
+This script contains necessary code to handle routes to /api/delete:
+
+   It handles DELETE requests to /api/delete/department/
+   It handles DELETE requests to /api/delete/role/
+	It handles DELETE requests to /api/delete/employee/
+
+Copyright Leo Wong 2022
+*/
