787bcbf114ad9343e8c56d4e5459fe955379c7c8
Leo Wong <leoelicos@gmail.com>
2022-05-12 00:15:39 +1000

    Add comment header

---
+++ utils.js/deleteFetch.js
@@ -0,0 +1,11 @@
+/* 
+deleteFetch.js
+
+This utility file contains helper DELETE fetch methods
+
+deleteDepartment(department_id)
+deleteRole(role_id)
+deleteEmployee(employee_id)
+
+Copyright Leo Wong 2022
+*/
