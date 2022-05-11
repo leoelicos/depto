3d51d76d8ce91accd16811e8309847183ff27954
Leo Wong <leoelicos@gmail.com>
2022-05-11 21:59:25 +1000

    Modularize route logic

--- routes/view.js
+++ routes/view.js
@@ -17,3 +17,8 @@ Copyright Leo Wong 2022
 const CREATED = 201;
 const BADREQUEST = 404;
 const NOTFOUND = 404;
+
+// modularize route logic. This utility is exported at the end of this file.
+const view = require('express').Router();
+
+module.exports = view;
