2885a08839cd6998ab49e6c506346abbc3aa1b77
Leo Wong <leoelicos@gmail.com>
2022-05-11 18:03:11 +1000

    Add comment header

---
+++ routes/add.js
@@ -0,0 +1,11 @@
+/* 
+add.js
+
+This script contains necessary code to handle routes to /api/add:
+
+	It handles POST 	requests to /api/add/department/
+	It handles POST 	requests to /api/add/role/
+	It handles POST 	requests to /api/add/employee/
+
+Copyright Leo Wong 2022
+*/
