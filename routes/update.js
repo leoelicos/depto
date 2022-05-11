7fd415220c1c515b4deefefe2c0782b3eda67d7e
Leo Wong <leoelicos@gmail.com>
2022-05-11 22:37:20 +1000

    Add comment header

---
+++ routes/update.js
@@ -0,0 +1,10 @@
+/* 
+update.js
+
+This script contains necessary code to handle routes to /api/update:
+
+	It handles PUT requests to /api/update/employeeRole/
+	It handles PUT requests to /api/update/employeeManager/
+
+Copyright Leo Wong 2022
+*/
