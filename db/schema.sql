bcc2a64bdea190ee0ebb42841fe285303458ec87
Leo Wong <leoelicos@gmail.com>
2022-05-11 14:40:34 +1000

    Add foreign key 'role_id' to employee table

--- db/schema.sql
+++ db/schema.sql
@@ -26,9 +26,12 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
+  role_id INT,
   manager_id INT,
-  FOREIGN KEY (manager_id)
-  REFERENCES employee(id)
+  FOREIGN KEY (role_id) 
+  REFERENCES role(id) 
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) 
+  REFERENCES employee(id) 
   ON DELETE SET NULL
 );
