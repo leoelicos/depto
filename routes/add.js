3a853af77ecd7c7260db38f51a4084bc343538f8
Leo Wong <leoelicos@gmail.com>
2022-05-11 20:48:01 +1000

    Route handler addDepartment

--- routes/add.js
+++ routes/add.js
@@ -19,10 +19,57 @@ const BADREQUEST = 400;
 // modularize route logic. This utility is exported at the end of this file.
 const add = require('express').Router();
 
-// implement middleware for parsing JSON
-app.use(express.json());
+// import SQL class
+const SQL = require('../utils/sql');
 
-// implement middleware for parsing urlencoded form data
-app.use(express.urlencoded({ extended: false }));
+// mysql2 is an npm library package which allows javascript access to an SQL database
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+	{
+		host: 'localhost',
+		// MySQL username,
+		user: 'root',
+		// TODO: Add MySQL password
+		password: 'xxxx',
+		database: 'cms',
+	},
+	console.log(`Connected to the cms database.`)
+);
+
+// implement route handler for POST request to /api/add/department
+add.post('/department', (req, res) => {
+	//
+	// check if request has a body and a name property inside the body
+	if (!req.body || !req.body.name) {
+		// respond to the POST request with status(BADREQUEST)
+		res.status(BADREQUEST).json({
+			error: err.message,
+			data: req,
+		});
+	}
+
+	// destructure name from the body
+	const { name } = req.body;
+	const sql = new SQL().addDepartment();
+	const params = name;
+	db.query(sql, params, (err, result) => {
+		if (err) {
+			// respond to the POST request with status(NOTFOUND)
+			res.status(NOTFOUND).json({
+				error: err.message,
+				data: req,
+			});
+		}
+
+		// respond to the POST request with status(CREATED)
+		res.status(CREATED).json({
+			message: 'Successfully added',
+			changes: result.affectedRows,
+			data: result,
+		});
+	});
+});
 
 module.exports = add;
