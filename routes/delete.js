18c41e12d92d4e4c9ec5fd9be16a5a717fc1903b
Leo Wong <leoelicos@gmail.com>
2022-05-11 23:15:35 +1000

    Add HTTP response status codes

--- routes/delete.js
+++ routes/delete.js
@@ -9,3 +9,8 @@ This script contains necessary code to handle routes to /api/delete:
 
 Copyright Leo Wong 2022
 */
+
+// HTTP response status codes: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+const CREATED = 201;
+const BADREQUEST = 400;
+const NOTFOUND = 404;
