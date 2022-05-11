3bdb7af5c05d1c66acee8be37ed4b9a2cf1b3ddd
Leo Wong <leoelicos@gmail.com>
2022-05-11 18:08:14 +1000

    Modularize route logic

--- routes/add.js
+++ routes/add.js
@@ -10,6 +10,9 @@ This script contains necessary code to handle routes to /api/add:
 Copyright Leo Wong 2022
 */
 
+// modularize route logic. This utility is exported at the end of this file.
+const add = require('express').Router();
+
 // implement middleware for parsing JSON
 app.use(express.json());
 
