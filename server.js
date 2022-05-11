f8ebf341d15fdee40e19ba9b58f39ce66060a78a
Leo Wong <leoelicos@gmail.com>
2022-05-11 17:22:44 +1000

    Add PORT

--- server.js
+++ server.js
@@ -11,3 +11,6 @@ const express = require('express');
 
 // mysql2 is an npm library package which allows javascript access to an SQL database
 const mysql = require('mysql2');
+
+// process.env.PORT is a requirement for Heroku deployment, in case we need to later on: https://help.heroku.com/P1AVPANS/why-is-my-node-js-app-crashing-with-an-r10-error
+const PORT = process.env.PORT || 3001;
