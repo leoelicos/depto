e2a098e1e23d9b6f0b17b883329e5c0f7e5ff5d7
Leo Wong <leoelicos@gmail.com>
2022-05-11 21:55:27 +1000

    Add comment header

---
+++ routes/view.js
@@ -0,0 +1,14 @@
+/* 
+view.js
+
+This script contains necessary code to handle routes to /api/view:
+
+	It handles GET requests to /api/view/departments/
+	It handles GET requests to /api/view/roles/
+	It handles GET requests to /api/view/employees/
+	It handles GET requests to /api/view/employeesByDepartment/:department_id
+	It handles GET requests to /api/view/employeesByManager/:manager_id
+	It handles GET requests to /api/view/totalUtilizedBudget/:department_id
+
+Copyright Leo Wong 2022
+*/
