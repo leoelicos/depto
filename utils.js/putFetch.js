384765d62fff345478662302df5ecc433c83db29
Leo Wong <leoelicos@gmail.com>
2022-05-12 00:10:18 +1000

    Add comment header

---
+++ utils.js/putFetch.js
@@ -0,0 +1,10 @@
+/* 
+putFetch.js
+
+This utility file contains helper PUT fetch methods
+
+updateEmployeeRole(employee_id, role_id)
+updateEmployeeManager(employee_id, manager_id)
+
+Copyright Leo Wong 2022
+*/
