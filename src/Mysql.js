c86162f3e87014a0b921fdd728f2ac6af42ce903
Leo Wong <leoelicos@gmail.com>
2022-05-15 12:02:31 +1000

    Adjust tabs

--- src/Mysql.js
+++ src/Mysql.js
@@ -1,27 +1,31 @@
 // import Connection class
 const db = require('../config/connection');
-
+const { quaternary } = require('./utils/chalkRender');
 class Mysql {
 	getDepartments = () =>
 		new Promise(function (resolve, reject) {
 			const sql = `
-		SELECT id, name AS department 
-		FROM department;
-		`;
+				SELECT id, name AS department 
+				FROM department;
+			`;
 			db.query(sql, (err, result) => {
-				if (err) reject(new Error(`Check your SQL: ${err}`));
-				if (result.length === 0) resolve(['No departments found']);
-				resolve(result);
+				if (err) {
+					reject(`Check your SQL: ${sql}\n${err}`);
+				} else if (result.length === 0) {
+					reject(''.padStart(4) + quaternary('No departments found'));
+				} else {
+					resolve(result);
+				}
 			});
 		});
 
 	getRoles = () =>
 		new Promise(function (resolve, reject) {
 			const sql = `
-			SELECT r.id, r.title, d.name AS department, r.salary
-			FROM role AS r
-			JOIN department AS d
-			ON r.department_id = d.id;
+				SELECT r.id, r.title, d.name AS department, r.salary
+				FROM role AS r
+				JOIN department AS d
+				ON r.department_id = d.id;
 			`;
 			db.query(sql, (err, result) => {
 				if (err) reject(new Error(`Check your SQL: ${err}`));
@@ -33,15 +37,15 @@ class Mysql {
 	getEmployees = () =>
 		new Promise(function (resolve, reject) {
 			const sql = `
-			SELECT e.id, e.first_name, e.last_name, title, name AS department, salary, 
-			CONCAT(e2.first_name,' ',e2.last_name) AS manager
-			FROM employee AS e
-			INNER JOIN role AS r
-			ON e.role_id = r.id
-			INNER JOIN department AS d
-			ON r.department_id = d.id
-			LEFT JOIN employee AS e2
-			ON e.manager_id = e2.id;
+				SELECT e.id, e.first_name, e.last_name, title, name AS department, salary, 
+				CONCAT(e2.first_name,' ',e2.last_name) AS manager
+				FROM employee AS e
+				INNER JOIN role AS r
+				ON e.role_id = r.id
+				INNER JOIN department AS d
+				ON r.department_id = d.id
+				LEFT JOIN employee AS e2
+				ON e.manager_id = e2.id;
 			`;
 			db.query(sql, (err, result) => {
 				if (err) reject(new Error(`Check your SQL: ${err}`));
@@ -53,18 +57,21 @@ class Mysql {
 	getManagers = () =>
 		new Promise(function (resolve, reject) {
 			const sql = `
-			SELECT e.id,  e.first_name, e.last_name, title, name AS department, salary, CONCAT(e2.first_name,' ',e2.last_name) AS manager 		     
-			FROM employee AS e
-			INNER JOIN role AS r
-			ON e.role_id = r.id
-			INNER JOIN department AS d
-			ON r.department_id = d.id
-			LEFT JOIN employee AS e2
-			ON e.manager_id = e2.id
-			WHERE e.id IN (SELECT e.manager_id		     
-			FROM employee AS e
-			LEFT JOIN employee AS e2
-			ON e.manager_id = e2.id);
+				SELECT e.id,  e.first_name, e.last_name, title, name AS department, salary, 
+					CONCAT(e2.first_name,' ',e2.last_name) AS manager 		     
+				FROM employee AS e
+				INNER JOIN role AS r
+					ON e.role_id = r.id
+				INNER JOIN department AS d
+					ON r.department_id = d.id
+				LEFT JOIN employee AS e2
+					ON e.manager_id = e2.id
+				WHERE e.id IN (
+					SELECT e.manager_id		     
+					FROM employee AS e
+					LEFT JOIN employee AS e2
+						ON e.manager_id = e2.id
+				);
 			`;
 			db.query(sql, (err, result) => {
 				if (err) reject(new Error(`Check your SQL: ${err}`));
@@ -76,18 +83,18 @@ class Mysql {
 	getEmployeesByDepartment = (department_id, departmentName) =>
 		new Promise(function (resolve, reject) {
 			const sql = `
-			SELECT e.id, e.first_name, e.last_name, title 
-			FROM employee AS e 
-			INNER JOIN role AS r 
-			ON e.role_id = r.id 
-			INNER JOIN department AS d
-			ON r.department_id = d.id
-			WHERE d.id = ?;
+				SELECT e.id, e.first_name, e.last_name, title 
+				FROM employee AS e 
+				INNER JOIN role AS r 
+					ON e.role_id = r.id 
+				INNER JOIN department AS d
+					ON r.department_id = d.id
+				WHERE d.id = ?;
 			`;
 			const params = department_id;
 			db.query(sql, params, (err, result) => {
 				if (err) reject(new Error(`Check your SQL: ${err}`));
-				if (result.length === 0) resolve([`No employees found in department ${departmentName}`]);
+				if (result.length === 0) reject([`No employees found in department ${departmentName}`]);
 				resolve(result);
 			});
 		});
@@ -95,15 +102,15 @@ class Mysql {
 	getEmployeesByManager = (manager_id, managerName) =>
 		new Promise(function (resolve, reject) {
 			const sql = `
-			SELECT e.id, e.first_name, e.last_name, title 
-			FROM employee AS e 
-			INNER JOIN role AS r 
-			ON e.role_id = r.id 
-			INNER JOIN department AS d
-			ON r.department_id = d.id
-			LEFT JOIN employee AS e2
-			on e.manager_id = e2.id
-			WHERE e.manager_id = ?;
+				SELECT e.id, e.first_name, e.last_name, title 
+				FROM employee AS e 
+				INNER JOIN role AS r 
+					ON e.role_id = r.id 
+				INNER JOIN department AS d
+					ON r.department_id = d.id
+				LEFT JOIN employee AS e2
+					ON e.manager_id = e2.id
+				WHERE e.manager_id = ?;
 			`;
 			const params = manager_id;
 			db.query(sql, params, (err, result) => {
@@ -116,13 +123,13 @@ class Mysql {
 	getTotalUtilizedBudget = (department_id) =>
 		new Promise(function (resolve, reject) {
 			const sql = `
-			SELECT d.name AS 'department', SUM(salary) AS 'total utilized budget'            
-			FROM employee AS e
-			INNER JOIN role AS r
-			ON e.role_id = r.id
-			INNER JOIN department AS d
-			ON r.department_id = d.id
-			WHERE r.department_id = ?;
+				SELECT d.name AS 'department', SUM(salary) AS 'total utilized budget'            
+				FROM employee AS e
+				INNER JOIN role AS r
+					ON e.role_id = r.id
+				INNER JOIN department AS d
+					ON r.department_id = d.id
+				WHERE r.department_id = ?;
 			`;
 			const params = department_id;
 			db.query(sql, params, (err, result) => {
@@ -134,8 +141,10 @@ class Mysql {
 
 	addDepartment = (name) =>
 		new Promise(function (resolve, reject) {
-			const sql = `INSERT INTO department (name)
-			VALUES (?);`;
+			const sql = `
+				INSERT INTO department (name)
+				VALUES (?);
+			`;
 			const params = name;
 			db.query(sql, params, (err, result) => {
 				if (err) reject(new Error(`Check your SQL: ${err}`));
@@ -146,8 +155,10 @@ class Mysql {
 
 	addRole = (title, salary, department_id) =>
 		new Promise(function (resolve, reject) {
-			const sql = `INSERT INTO role (title, salary, department_id)
-			VALUES (?, ?, ?);`;
+			const sql = `
+				INSERT INTO role (title, salary, department_id)
+				VALUES (?, ?, ?);
+			`;
 			const params = [title, salary, department_id];
 			db.query(sql, params, (err, result) => {
 				if (err) reject(new Error(`Check your SQL: ${err}`));
@@ -158,8 +169,10 @@ class Mysql {
 
 	addEmployee = (first_name, last_name, role_id, manager_id) =>
 		new Promise(function (resolve, reject) {
-			const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
-			VALUES (?, ?, ?, ?);`;
+			const sql = `
+				INSERT INTO employee (first_name, last_name, role_id, manager_id)
+				VALUES (?, ?, ?, ?);
+			`;
 			const params = [first_name, last_name, role_id, manager_id];
 			db.query(sql, params, (err, result) => {
 				if (err) reject(new Error(`Check your SQL: ${err}`));
@@ -170,7 +183,11 @@ class Mysql {
 
 	updateEmployeeRole = (employee_id, role_id, employeeName, roleName) =>
 		new Promise(function (resolve, reject) {
-			const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+			const sql = `
+				UPDATE employee 
+				SET role_id = ? 
+				WHERE id = ?
+			`;
 			const params = [role_id, employee_id];
 			db.query(sql, params, (err, result) => {
 				if (err) reject(new Error(`Check your SQL: ${err}`));
@@ -182,7 +199,11 @@ class Mysql {
 
 	updateEmployeeManager = (employee_id, manager_id, employeeName, managerName) =>
 		new Promise(function (resolve, reject) {
-			const sql = `UPDATE employee SET manager_id = ? WHERE id = ?`;
+			const sql = `
+				UPDATE employee
+				SET manager_id = ?
+				WHERE id = ?
+			`;
 			const params = [manager_id, employee_id];
 			db.query(sql, params, (err, result) => {
 				if (err) reject(new Error(`Check your SQL: ${err}`));
@@ -194,7 +215,11 @@ class Mysql {
 
 	deleteDepartment = (department_id, departmentName) =>
 		new Promise(function (resolve, reject) {
-			const sql = `DELETE FROM department WHERE id = ?`;
+			const sql = `
+				DELETE 
+				FROM department 
+				WHERE id = ?
+			`;
 			const params = department_id;
 			db.query(sql, params, (err, result) => {
 				if (err) reject(new Error(`Check your SQL: ${err}`));
@@ -205,7 +230,11 @@ class Mysql {
 
 	deleteRole = (role_id, roleName) =>
 		new Promise(function (resolve, reject) {
-			const sql = `DELETE FROM role WHERE id = ?`;
+			const sql = `
+				DELETE 
+				FROM role
+				WHERE id = ?
+			`;
 			const params = role_id;
 			db.query(sql, params, (err, result) => {
 				if (err) reject(new Error(`Check your SQL: ${err}`));
@@ -216,7 +245,10 @@ class Mysql {
 
 	deleteEmployee = (employee_id, employeeName) =>
 		new Promise(function (resolve, reject) {
-			const sql = `DELETE FROM employee WHERE id = ?`;
+			const sql = `
+				DELETE FROM employee 
+				WHERE id = ?
+			`;
 			const params = employee_id;
 			db.query(sql, params, (err, result) => {
 				if (err) reject(new Error(`Check your SQL: ${err}`));
