e58bb81ef51a57e0b465c68eda4fe5cb59418956
Leo Wong <leoelicos@gmail.com>
2022-05-15 21:45:50 +1000

    Add function to disconnect from database

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,14 @@
+/*
+ * Employee Management System
+ * connection.js
+ * This script connects to the local mysql database
+ * Copyright 2022 Leo Wong
+ */
+
 // mysql2 is an npm library package which allows javascript access to an SQL database
 const mysql2 = require('mysql2');
 
+// create connection to local mysql database
 const db = mysql2.createConnection({
 	host: 'localhost',
 	user: 'root',
@@ -8,4 +16,7 @@ const db = mysql2.createConnection({
 	database: 'ems',
 });
 
-module.exports = db;
+const disconnect = async () => db.end();
+
+// export connection
+module.exports = { db, disconnect };
