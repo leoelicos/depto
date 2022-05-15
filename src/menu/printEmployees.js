b18ed0de689b00dc56e9812568dcaede709edbd8
Leo Wong <leoelicos@gmail.com>
2022-05-15 21:51:49 +1000

    Import sqlGetEmployees

---
+++ src/menu/printEmployees.js
@@ -0,0 +1,31 @@
+/*
+ * Employee Management System
+ * printEmployees.js
+ * this file contains a function that can print all employees from the database
+ * Copyright 2022 Leo Wong
+ */
+
+const { sqlGetEmployees } = require('../mysql2');
+
+/*
+ * Function to print all employees from the database
+ * mysql 	> get the list of employees from the database
+ */
+printEmployees = async () => {
+	try {
+		//* mysql 	> get the list of employees from the database
+		const eObjects = await sqlGetEmployees();
+
+		// log view title
+		console.log('\nTable: All employees');
+
+		// log view
+		console.table(eObjects);
+		//
+	} catch (err) {
+		// handle errors
+		console.error(err);
+	}
+};
+
+module.exports = { printEmployees };
