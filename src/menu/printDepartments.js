c2858aa685a09c264c5b48a6c6fc610d04471046
Leo Wong <leoelicos@gmail.com>
2022-05-15 21:51:42 +1000

    Import sqlGetDepartments

---
+++ src/menu/printDepartments.js
@@ -0,0 +1,30 @@
+/*
+ * Employee Management System
+ * printDepartments.js
+ * this file contains a function that can print all departments from the database
+ * Copyright 2022 Leo Wong
+ */
+
+const { sqlGetDepartments } = require('../mysql2');
+
+/*
+ * Function to print all departments from the database
+ * mysql 	> get the list of departments from the database
+ */
+printDepartments = async () => {
+	try {
+		//* mysql 	> get the list of departments from the database
+		const dObjects = await sqlGetDepartments();
+
+		// log view title
+		console.log('\nTable: All Departments');
+
+		// log view
+		console.table(dObjects);
+		//
+	} catch (err) {
+		// handle errors
+		console.error(err);
+	}
+};
+module.exports = { printDepartments };
