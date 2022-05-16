72053d0691e95fc688a199d89eb1430b7ea26966
Leo Wong <leoelicos@gmail.com>
2022-05-16 13:00:05 +1000

    Simplify connection syntax

--- config/connection.js
+++ config/connection.js
@@ -6,15 +6,15 @@
  */
 
 // mysql2 is an npm library package which allows javascript access to an SQL database
-const mysql2 = require('mysql2');
-
-// create connection to local mysql database
-const db = mysql2.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'xxxx',
-	database: 'ems',
-});
+const db = require('mysql2').createConnection(
+	// database location
+	{
+		host: 'localhost',
+		user: 'root',
+		password: 'xxxx',
+		database: 'ems',
+	}
+);
 
 const disconnect = async () => db.end();
 
