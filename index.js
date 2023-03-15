20010f80d6714fab0c342693b59a42e5f8e2dd09
Leo Wong <leoelicos@gmail.com>
2023-03-16 04:01:23 +1100

    Remove comments, add dotenv and cors. Remove CLI functions

--- index.js
+++ index.js
@@ -1,117 +1,26 @@
-/*
- * Employee Management System
- * index.js
- * this file runs the system by repeatedly prompting the user for menu actions
- * Copyright 2022 Leo Wong
- */
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-//* import inquirer to handle prompts
-const inquirer = require('inquirer');
+import express from 'express'
+import cors from 'cors'
 
-// utility function to create good-looking console logs
-const { primary, secondary } = require('./src/utils/chalkRender');
+import db from './config/connection.js'
+import routes from './routes/index.js'
 
-//* inquirer function that asks user which menu action they would like
-const inquireMenu = () =>
-	inquirer.prompt([
-		{
-			name: 'menu',
-			type: 'list',
-			prefix: ' ' + primary('Menu'.padEnd(120)) + '\n',
-			message: secondary('What would you like to do?'.padEnd(120)) + '\n > ',
-			choices: ['View All Departments', 'View All Roles', 'View All Employees', 'View Employees by Department', 'View Employees by Manager', 'View Total Utilized Budget by Department', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Update Employee Manager', 'Delete Department', 'Delete Role', 'Delete Employee', 'Quit'],
-			pageSize: 5,
-		},
-	]);
+const app = express()
 
-// CRUD
+app.use(cors())
 
-//* import create scripts
-const { addDepartment } = require('./src/crud/addDepartment.js');
-const { addEmployee } = require('./src/crud/addEmployee.js');
-const { addRole } = require('./src/crud/addRole.js');
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-//* import read scripts
-const { printDepartments } = require('./src/crud/printDepartments.js');
-const { printEmployees } = require('./src/crud/printEmployees.js');
-const { printEmployeesByDepartment } = require('./src/crud/printEmployeesByDepartment.js');
-const { printEmployeesByManager } = require('./src/crud/printEmployeesByManager.js');
-const { printRoles } = require('./src/crud/printRoles.js');
-const { printUtilizedBudgetByDepartment } = require('./src/crud/printUtilizedBudgetByDepartment.js');
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+})
+app.use(routes)
+const PORT = process.env.port || 3001
 
-//* import update scripts
-const { updateEmployeeManager } = require('./src/crud/updateEmployeeManager.js');
-const { updateEmployeeRole } = require('./src/crud/updateEmployeeRole.js');
-
-//* import delete scripts
-const { deleteDepartment } = require('./src/crud/deleteDepartment.js');
-const { deleteEmployee } = require('./src/crud/deleteEmployee.js');
-const { deleteRole } = require('./src/crud/deleteRole.js');
-
-//* import disconnect function
-const { disconnect } = require('./config/connection');
-
-//* import utility function to render user messages
-const { logoHelper } = require('./src/utils/chalkRender');
-
-// Function to prompt the user for actions
-const ems = async () => {
-	// prompt user for menu option
-	let { menu } = await inquireMenu();
-	// base case
-	if (menu === 'Quit') {
-		return;
-	} else if (menu === 'View All Departments') {
-		await printDepartments();
-	} else if (menu === 'View All Roles') {
-		await printRoles();
-	} else if (menu === 'View All Employees') {
-		await printEmployees();
-	} else if (menu === 'View Employees by Department') {
-		await printEmployeesByDepartment();
-	} else if (menu === 'View Employees by Manager') {
-		await printEmployeesByManager();
-	} else if (menu === 'View Total Utilized Budget by Department') {
-		await printUtilizedBudgetByDepartment();
-	} else if (menu === 'Add Department') {
-		await addDepartment();
-	} else if (menu === 'Add Role') {
-		await addRole();
-	} else if (menu === 'Add Employee') {
-		await addEmployee();
-	} else if (menu === 'Update Employee Role') {
-		await updateEmployeeRole();
-	} else if (menu === 'Update Employee Manager') {
-		await updateEmployeeManager();
-	} else if (menu === 'Delete Department') {
-		await deleteDepartment();
-	} else if (menu === 'Delete Role') {
-		await deleteRole();
-	} else if (menu === 'Delete Employee') {
-		await deleteEmployee();
-	}
-	await ems();
-};
-
-// function to start the application
-const init = async () => {
-	//
-	// welcome message
-	logoHelper('', ' ');
-	logoHelper('', '*');
-	logoHelper(' EMPLOYEE MANAGEMENT SYSTEM ', ' ');
-	logoHelper('', '*');
-	logoHelper('', ' ');
-
-	// start the menu and wait for it to finish
-	await ems();
-
-	// disconnect from server
-	await disconnect();
-
-	// goodbye message
-	logoHelper('Thank you for using Employee Management System.');
-};
-
-// start the application
-init();
+app.listen(PORT, () => {
+  console.log(`API server  running on port ${PORT}!`)
+})
